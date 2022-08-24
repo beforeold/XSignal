0d2ffe95dda7cf0f569af331b96a6bfc788fb8aa
beforeold <ibeforeold@gmail.com>
2022-08-25 01:39:48 +0800

    update folder

---
+++ XSignal/XSignal.h
@@ -0,0 +1,18 @@
+//
+//  XSignal.h
+//  TestMySignal
+//
+//  Created by beforeold on 2022/8/25.
+//  Copyright © 2022 Brook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#import "XSGGenerator.h"
+#import "XSGSubscriber.h"
+#import "XSGGenerator+XSGOperators.h"
+#import "UIButton+XSGSupport.h"
+
+NS_ASSUME_NONNULL_END
